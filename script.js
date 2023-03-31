3c89638466f946a09698b7f06d74b5f229fe9a30
Joao-Dev-Pedro <joaoalldesign@gmail.com>
2023-03-30 21:32:35 -0300

    att

---
+++ script.js
@@ -0,0 +1,13 @@
+var menu = document.getElementById('menu-list')
+
+
+
+
+function openMenu(){
+   menu.classList.replace('menu-itens-close', 'menu-open-clik')
+ 
+}
+//function closeMenu(){
+ //   menu.style.display='none'
+
+//}
